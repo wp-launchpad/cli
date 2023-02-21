4f8fcc4331aa63ebf12798ec5d1eeeac362ebb75
cyrille <coquardcyr@gmail.com>
2023-02-22 00:23:09 +0100

    Added a way to get parameters and if a function has a return value

--- inc/Services/FixtureGenerator.php
+++ inc/Services/FixtureGenerator.php
@@ -30,14 +30,37 @@ class FixtureGenerator
     }
 
     protected function has_method(string $method, string $content) {
-
+        return ( ! preg_match("/public[ \n]+function[ \n]+$method/", $content ) );
     }
 
     protected function get_parameters(string $method, string $content) {
+        if ( ! preg_match("/public[ \n]+function[ \n]+{$method}[ \n]*\((?<parameters>[^\)])*\)/", $content, $results ) ) {
+            return [];
+        }
+        $parameters = $results['parameters'];
 
+        if(! preg_match_all('/(?<type>\w+)?[ \n]+(?<name>\$\w+)/m', $parameters, $results) ||
+            !key_exists('name', $results)) {
+            return [];
+        }
+        $types = $results['type'];
+        $names = $results['name'];
+
+        $ouput = array_combine($names, $types);
+
+        if(! $ouput) {
+            return [];
+        }
+
+        return $ouput;
     }
 
     protected function has_return(string $method, string $content) {
+        if(! preg_match("/public[ \n]+function[ \n]+{$method}[ \n]*\(([^\)])*\)[ \n]*(?<content>?:[^{}]+|\{(?1)\})*/", $content, $results ) ) {
+            return false;
+        }
+        $content = $results['content'];
 
+        return preg_match('/return\s+([^;]+);$/', $content);
     }
 }
